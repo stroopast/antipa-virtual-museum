5bff6e0fdb555b6d122fd347545a93848893bb7f
Roberto <nanuroberto20@gmail.com>
2025-04-28 19:56:37 +0300

    Organize gameobjects in scene based on categories

--- Assets/Scripts/CameraScripts/CameraScript.cs
+++ Assets/Scripts/CameraScripts/CameraScript.cs
@@ -1,15 +1,21 @@
-using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    void Start()
+    private void Start()
+    {
+        AttachCameraToPlayer();
+    }
+
+    private void AttachCameraToPlayer()
     {
         Transform player = GameObject.FindWithTag("Player").GetComponent<Transform>();
         Transform lookAtTarget = player.transform.Find("LookAt");
         gameObject.GetComponent<CinemachineFreeLook>().Follow = player;
         gameObject.GetComponent<CinemachineFreeLook>().LookAt = lookAtTarget;
+        //gameObject.GetComponent<CinemachineFreeLook>(). = lookAtTarget.GetComponent<Transform>();
     }
 }
