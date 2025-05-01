fcfff7263a0fa7d4005a5b693095155f076cb761
stroopast <nanuroberto20@gmail.com>
2025-05-01 10:54:06 +0300

    Solve camera issue attaching only to the Host player on every client instance

--- Assets/Scripts/CameraScripts/CameraScript.cs
+++ Assets/Scripts/CameraScripts/CameraScript.cs
@@ -1,20 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
+using Unity.Netcode;
 using UnityEngine;
 
-public class CameraScript : MonoBehaviour
+public class CameraScript : NetworkBehaviour
 {
+    private CinemachineFreeLook FreeLookCamera;
     private void Start()
     {
-        //AttachCameraToPlayer();
+        AttachCameraToPlayer();
     }
 
     public void AttachCameraToPlayer()
     {
-        Transform player = GameObject.FindWithTag("Player").GetComponent<Transform>();
-        Transform lookAtTarget = player.transform.Find("LookAt");
-        gameObject.GetComponent<CinemachineFreeLook>().Follow = player;
-        gameObject.GetComponent<CinemachineFreeLook>().LookAt = lookAtTarget;
+        if(!IsOwner) { return; }
+        FreeLookCamera = GameObject.FindWithTag("FreeLookCamera").GetComponent<CinemachineFreeLook>();
+        FreeLookCamera.Follow = gameObject.GetComponent<Transform>();
+        FreeLookCamera.LookAt = gameObject.GetComponent<Transform>().Find("LookAt");
+        //Transform player = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        //Transform lookAtTarget = player.transform.Find("LookAt");
+        //gameObject.GetComponent<CinemachineFreeLook>().Follow = player;
+        //gameObject.GetComponent<CinemachineFreeLook>().LookAt = lookAtTarget;
     }
+
 }
