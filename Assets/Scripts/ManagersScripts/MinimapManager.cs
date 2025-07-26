3ab0812ac5a0d76456efdf944c37bbb686c0a481
Roberto <nanuroberto20@gmail.com>
2025-07-26 18:44:24 +0300

    Add minimap, add symbols for each rom on minimap

---
+++ Assets/Scripts/ManagersScripts/MinimapManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapManager : MonoBehaviour
+{
+    [SerializeField] private GameObject minimapUI;
+
+    void Update()
+    {
+        GameObject[] menuObjects = GameObject.FindGameObjectsWithTag("Menu");
+
+        bool isMenuActive = false;
+
+        foreach (GameObject menu in menuObjects)
+        {
+            if (menu.activeInHierarchy)
+            {
+                isMenuActive = true;
+                break;
+            }
+        }
+
+        minimapUI.SetActive(!isMenuActive);
+    }
+}
